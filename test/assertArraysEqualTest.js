ef24c5f80c369f2a481ec574f1fd8757b62f8086
coding4rever <propertyvaluecss@gmail.com>
2023-05-25 03:59:46 +0100

    Lotide Project

--- test/assertArraysEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertArraysEqualTest = require(`../assertArraysEqual`)
-
-
-
-console.log(assertArraysEqualTest(([1, 2, 3], [1, 2, 3]) , false));
-console.log(assertArraysEqualTest(([1, 2, 3], []) , false));
--- test/eqArraysTest.js
+++
@@ -1,6 +0,0 @@
-const eqArraysTest = require('../eqArrays');
-
-
-
-console.log(eqArraysTest([1, 2, 3], [1, 2, 3]))
-console.log(eqArraysTest([1, 2, 3], []))
